ac7ff5ad58c27de58a4c8f12bc36607c4ada14fa
John <jgk305@nyu.edu>
2019-01-18 22:36:51 -0500

    Test Scripts and Scenes for App Functionality

---
+++ HololensPartAssets/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject Model;
+	public ImageTargetBehaviour spawnedITB;
+
+	public ImageTargetBehaviour currITB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (currITB.TrackableName != spawnedITB.TrackableName) {
+			SpawnModelScript SMS = Model.GetComponent("SpawnModelScript") as SpawnModelScript;
+			SMS.spawned = false;
+			Destroy(Model);
+		}	
+	}
+}
---
+++ HololensPartAssets/SpawnModelScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SpawnModelScript : MonoBehaviour {
+
+	public ImageTargetBehaviour ITB;
+	public GameObject Model;
+
+	public SpawnManager SM;
+
+	public bool spawned = false; // make the model spawn only once
+
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ITB.CurrentStatus == ImageTargetBehaviour.Status.TRACKED) {
+			SM.currITB = ITB;
+			if (spawned == false) {
+				Instantiate (Model, new Vector3 (0, 0, 3), new Quaternion (0, 0, 0, 0));
+				SM.spawnedITB = ITB;
+				SM.Model = GameObject.FindGameObjectWithTag("Model");
+				spawned = true;
+			}
+
+		}
+	}
+}
