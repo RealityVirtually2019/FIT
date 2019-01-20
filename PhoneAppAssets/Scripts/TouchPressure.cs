0d348b4c675b28e1a52f0b138cbdeb8509fa1dc2
Minerstudio <45796624+Minerstudio@users.noreply.github.com>
2019-01-20 12:45:41 -0500

    Added Touch Pressure Script

---
+++ PhoneAppAssets/Scripts/TouchPressure.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchPressure : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void Pressure()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).pressure <= 0.66f)
+            {
+                //gameObject.GetComponent<Image> ().color = Color.red;
+                print("ok");
+            }
+        }
+    }
+}
